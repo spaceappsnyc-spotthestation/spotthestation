630dc0ca486b8547ea433cb99b046f33db33b2de
Don Pflaster <dpflaster@gmail.com>
2013-04-20 15:25:37 -0400

    Rails Generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spotthestation::Application.config.secret_token = 'f71736d2965d28f93ba583efa3d2c98ed2e6e3deaded93f661586bec085002dea8606f7cd8b2908a6e3a35cbbd318a50967de8b45fb0a166f0a6eae2c9cd994a'
